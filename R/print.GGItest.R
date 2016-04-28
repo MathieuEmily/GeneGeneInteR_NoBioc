7e4e9c260eb96c4105d4ca6b4c896195afae63b5
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-04-28 14:44:13 +0200

    Customized print

---
+++ R/print.GGItest.R
@@ -0,0 +1,9 @@
+print.GGItest <- function(x){
+	cat(x$method,"\n")
+	if (is.null(x$df)){
+		cat(names(x$statistic)," = ",x$statistic,", p-value = ",x$p.value,"\n",sep="")
+	} else {
+		cat(names(x$statistic)," = ",x$statistic,", df = ",x$df,", p-value = ",x$p.value,"\n",sep="")	
+		}
+}
+

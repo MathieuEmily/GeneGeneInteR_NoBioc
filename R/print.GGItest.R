cbdc34baaa3f2b0b1e358420df26f318c4409fec
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-05-02 16:24:14 +0200

    Class GGItest

--- R/print.GGItest.R
+++ R/print.GGItest.R
@@ -1,9 +1,8 @@
 print.GGItest <- function(x){
-	cat(x$method,"\n")
+	cat("Gene-Gene Interaction method performed with:\n \t",x$method,"\n")
 	if (is.null(x$df)){
 		cat(names(x$statistic)," = ",x$statistic,", p-value = ",x$p.value,"\n",sep="")
 	} else {
 		cat(names(x$statistic)," = ",x$statistic,", df = ",x$df,", p-value = ",x$p.value,"\n",sep="")	
 		}
 }
-

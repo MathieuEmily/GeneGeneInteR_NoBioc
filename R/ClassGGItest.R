ca49dff78790b5c23dcc4d95012aa5ea22613078
MathieuEmily <memily@MacBook-Pro-de-memily-mbp.local>
2016-05-02 16:22:27 +0200

    Class GGItest

---
+++ R/ClassGGItest.R
@@ -0,0 +1,29 @@
+print.GGItest <- function(x){
+	cat("Gene-Gene Interaction method performed with:\n \t",x$method,"\n")
+	if (is.null(x$df)){
+		cat(names(x$statistic)," = ",x$statistic,", p-value = ",x$p.value,"\n",sep="")
+	} else {
+		cat(names(x$statistic)," = ",x$statistic,", df = ",x$df,", p-value = ",x$p.value,"\n",sep="")	
+		}
+}
+
+
+summary.GGItest <- function(x){
+	cat(x$method,"\n")
+	if (is.null(x$df)){
+		cat(names(x$statistic)," = ",x$statistic,", p-value = ",x$p.value,"\n",sep="")
+	} else {
+		cat(names(x$statistic)," = ",x$statistic,", df = ",x$df,", p-value = ",x$p.value,"\n",sep="")	
+		}
+	ll <- x$parameter
+	if (!is.null(x$parameter)){
+		for (i in 1:length(ll)){
+			cat(names(ll)[i],"=")
+			cat(ll[[i]])
+			if (i != length(ll)){
+			cat(" - ")
+			}
+		}
+	}
+}
+
